e866d438abd1e2bd693a4f22b0b7b7e40a6e9837
Olle Irenius <olle@nectima.se>
2018-03-21 13:51:54 +0100

    Fix syntax

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE := dlib
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../dlib
 
 LOCAL_SRC_FILES += \
-                ../$(LOCAL_PATH)/../dlib//dlib/threads/threads_kernel_shared.cpp \
+                ../$(LOCAL_PATH)/../dlib/dlib/threads/threads_kernel_shared.cpp \
                 ../$(LOCAL_PATH)/../dlib/dlib/entropy_decoder/entropy_decoder_kernel_2.cpp \
                 ../$(LOCAL_PATH)/../dlib/dlib/base64/base64_kernel_1.cpp \
                 ../$(LOCAL_PATH)/../dlib/dlib/threads/threads_kernel_1.cpp \
